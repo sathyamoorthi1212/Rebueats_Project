a501a64f89ad7bb9902d43d5ea6f6a1a6defe8b7
sathyamoorthi1212 <48886108+sathyamoorthi1212@users.noreply.github.com>
2022-04-27 10:32:48 +0530

    Add files via upload

---
+++ router/DriverRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport');
+const DriverController = require('../controllers/Driver/DriverController');
+const driverValidator = require('../validators/DriverValidator');
+const DriverAllocationController = require('../controllers/Driver/DriverAllocationController');
+const router = express.Router();
+const passportAuth = passport.authenticate("jwt", { session: false });
+const validate = require('express-validation');
+
+/*Driver Account*/
+
+router.route('/login').get(passportAuth, DriverController.currentLogin).post(validate(driverValidator.validation.login), DriverController.login);
+router.route('/signup').post(DriverController.profileUpload, DriverController.signup);
+router.route('/update/:id').put(/*validate(userValidator.validation.update),*/DriverController.profileUpload, DriverController.update);
+router.route('/getUsers').get(passportAuth, DriverController.getUsers);
+router.route('/logout/:id').post(passportAuth, DriverController.logout);
+router.route('/otp-verification/:id').post(/*passportAuth*/validate(driverValidator.validation.otpVerification), DriverController.otpVerification);
+router.route('/resend-otp/:id').post(/*passportAuth*/DriverController.ResendOtp);
+router.route('/changePassword/:id').post(passportAuth, validate(driverValidator.validation.changePassword), passportAuth, DriverController.changePassword);
+router.route('/forgetPassword/:email').post(/* passportAuth, */ DriverController.forgetPassword);
+router.route('/changeDriverStatus/:driverId').post(passportAuth, validate(driverValidator.validation.changeStatus), DriverController.statusChange);
+/*Driver Allocation Routes*/
+router.route('/assign/driverId/:driverId/orderId/:orderId').post(passportAuth, DriverAllocationController.assign);
+router.route('/autoAssign/orderId/:orderId').post(passportAuth, DriverAllocationController.autoAssign);
+router.route('/order/accept/driverId/:driverId/orderId/:orderId').post(passportAuth, DriverAllocationController.orderAccept);
+router.route('/order/reject/driverId/:driverId/orderId/:orderId').post(passportAuth, DriverAllocationController.orderReject);
+router.route('/order/change-status/:orderId').post(passportAuth, DriverAllocationController.changeOrderStatus);
+
+module.exports = router;
